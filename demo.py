da150df7f8060e112ae97baf7eb247499b9911b4
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-01-05 23:10:02 +0100

    Reuse one shared session-object across code (good idea?)

--- demo.py
+++ demo.py
@@ -24,7 +24,9 @@ logger.remove()
 logger.add(sys.stdout, level=log_level, diagnose=True, backtrace=False, enqueue=True)
 
 
-async def request_simulation(body: dict, q: asyncio.Queue):
+async def request_simulation(
+    session: aiohttp.ClientSession, body: dict, q: asyncio.Queue
+):
     """Request simulation by POSTing to `/experiments`."""
 
     id = body["modelInstanceID"]
@@ -32,19 +34,20 @@ async def request_simulation(body: dict, q: asyncio.Queue):
     href = f"{origin}/experiments"
     logger.trace(f"POST {href}")
 
-    async with aiohttp.ClientSession() as session:
-        # Trigger simulation and wait for 201
-        async with session.post(href, json=body) as res:
-            href_location = res.headers["Location"]
-            logger.debug(f"Location: {href_location}")
+    # Trigger simulation and wait for 201
+    async with session.post(href, json=body) as res:
+        href_location = res.headers["Location"]
+        logger.debug(f"Location: {href_location}")
 
-            logger.info(f"Requested simulation of model instance <{id}>")
+        logger.info(f"Requested simulation of model instance <{id}>")
 
-            # Enqueue link to resource just created
-            await q.put((id, href_location))
+        # Enqueue link to resource just created
+        await q.put((id, href_location))
 
 
-async def poll_until_done(q1: asyncio.Queue, q2: asyncio.Queue):
+async def poll_until_done(
+    session: aiohttp.ClientSession, q1: asyncio.Queue, q2: asyncio.Queue
+):
     """Poll specific experiment until it's done or failed."""
 
     counter_max = int(os.getenv("UC1D_POLLING_RETRIES", "30"))
@@ -55,25 +58,24 @@ async def poll_until_done(q1: asyncio.Queue, q2: asyncio.Queue):
         id, href = await q1.get()
 
         # Poll status of simulation
-        async with aiohttp.ClientSession() as session:
-            counter = 0
-            href_result = None
-            while counter < counter_max:
-                logger.trace(f"GET {href}")
-                async with session.get(href) as res:
-                    rep = await res.json()
-                    status = rep["status"]
-                    logger.trace(f"    {status}")
+        counter = 0
+        href_result = None
+        while counter < counter_max:
+            logger.trace(f"GET {href}")
+            async with session.get(href) as res:
+                rep = await res.json()
+                status = rep["status"]
+                logger.trace(f"    {status}")
 
-                    if status == "DONE":
-                        href_result = rep["linkToResult"]
-                        break
-                    if status == "FAILED":
-                        logger.warning("Simulation failed")
-                        break
+                if status == "DONE":
+                    href_result = rep["linkToResult"]
+                    break
+                if status == "FAILED":
+                    logger.warning("Simulation failed")
+                    break
 
-                    counter += 1
-                    await asyncio.sleep(freq)
+                counter += 1
+                await asyncio.sleep(freq)
 
         # Enqueue link to result
         await q2.put((id, href_result))
@@ -82,19 +84,18 @@ async def poll_until_done(q1: asyncio.Queue, q2: asyncio.Queue):
         q1.task_done()
 
 
-async def fetch_simulation_result(q: asyncio.Queue):
+async def fetch_simulation_result(session: aiohttp.ClientSession, q: asyncio.Queue):
     """Get the simulation result and parse it as dataframe."""
 
     while True:
         # Retrieve first item from queue
         id, href = await q.get()
 
-        async with aiohttp.ClientSession() as session:
-            # Get simulation result
-            logger.trace(f"GET {href}")
-            async with session.get(href) as res:
-                rep = await res.json()
-                # logger.trace(json.dumps(rep, indent=JSON_DUMPS_INDENT))
+        # Get simulation result
+        logger.trace(f"GET {href}")
+        async with session.get(href) as res:
+            rep = await res.json()
+            # logger.trace(json.dumps(rep, indent=JSON_DUMPS_INDENT))
 
         # # Parse as dataframe
         # logger.info(f"Result of simulation added to dataframe")
@@ -113,31 +114,35 @@ async def ensemble_forecast():
     }
     ensemble_runs_total = len(bc)
 
-    # Spawn task queues, producer and workers
+    # Spawn client session, task queues, producer and workers
+    session = aiohttp.ClientSession()
     q_sim = asyncio.Queue()
     q_res = asyncio.Queue()
 
     requesting = [
-        asyncio.create_task(request_simulation(body, q_sim)) for id, body in bc.items()
+        asyncio.create_task(request_simulation(session, body, q_sim))
+        for id, body in bc.items()
     ]
     polling = [
-        asyncio.create_task(poll_until_done(q_sim, q_res))
+        asyncio.create_task(poll_until_done(session, q_sim, q_res))
         for n in range(ensemble_runs_total)
     ]
     fetching = [
-        asyncio.create_task(fetch_simulation_result(q_res))
+        asyncio.create_task(fetch_simulation_result(session, q_res))
         for n in range(ensemble_runs_total)
     ]
 
     # Await addition of all tasks to the first queue
     await asyncio.gather(*requesting)
 
-    # Wait until the queues are fully processed; then cancel worker tasks
+    # Wait until the queues are fully processed; then cancel worker tasks&close session
     await q_sim.join()
     await q_res.join()
 
     for task in polling + fetching:
         task.cancel()
+
+    await session.close()
 
 
 if __name__ == "__main__":
