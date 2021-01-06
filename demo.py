cc679f64f164b742cab9d5e191e45f533777f848
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-01-06 21:41:17 +0100

    Get all model runs of a forecast model for a certain period; store as df

--- demo.py
+++ demo.py
@@ -8,11 +8,14 @@
 """Demonstrate performance of SIMaaS-implementation using async IO."""
 
 import asyncio
+import csv
 import json
 import os
 import sys
 
 import aiohttp
+import pandas as pd
+import pendulum
 from codetiming import Timer
 from loguru import logger
 
@@ -22,6 +25,42 @@ JSON_DUMPS_INDENT = 2  # intendation width for stringified JSON in spaces
 log_level = os.getenv("UC1D_LOG_LEVEL", "INFO")
 logger.remove()
 logger.add(sys.stdout, level=log_level, diagnose=True, backtrace=False, enqueue=True)
+
+
+def timeseries_array_as_df(body: dict):
+    """Parse timeseries-objects as pd.DataFrame."""
+
+    tmp_dfs = []
+    for result in body["data"]:
+        tmp = pd.read_json(json.dumps(result["timeseries"]))
+        tmp = tmp.set_index("timestamp")
+        tmp[f"{result['label']}_unit"] = result["unit"]
+        tmp.rename(columns={"value": result["label"]}, inplace=True)
+        logger.trace(f"tmp\n{tmp}")
+
+        tmp_dfs.append(tmp)
+
+    if len(tmp_dfs) > 1:
+        df = tmp_dfs[0].join(tmp_dfs[1:], how="outer")
+    else:
+        df = tmp_dfs[0]
+
+    logger.trace(f"df\n{df}")
+    return df
+
+
+async def request_weather_forecast(
+    session: aiohttp.ClientSession, station_id: str, params: dict, wfc_raw
+):
+    origin = os.getenv("UC1D_WEATHER_API_ORIGIN")  # TODO throw if not set
+    href = f"{origin}/weather-stations/{station_id}/forecast"
+
+    async with session.get(href, params=params) as res:
+        logger.trace(f"GET {res.url}")
+        rep = await res.json()
+        logger.trace(json.dumps(rep, indent=JSON_DUMPS_INDENT))
+
+        wfc_raw.append((params["model-run"], rep))
 
 
 async def request_simulation(
@@ -104,124 +143,76 @@ async def fetch_simulation_result(session: aiohttp.ClientSession, q: asyncio.Que
         q.task_done()
 
 
-def get_simulation_request_bodies():
-    eg_body = {
-        "modelInstanceID": "c02f1f12-966d-4eab-9f21-dcf265ceac71",
-        "simulationParameters": {
-            "startTime": 1542412800000,
-            "stopTime": 1542499199999,
-            "outputInterval": 3600,
-        },
-        "inputTimeseries": [
-            {
-                "label": "temperature",
-                "unit": "K",
-                "timeseries": [
-                    {"timestamp": 1542412800000, "value": 274.6336669921875},
-                    {"timestamp": 1542416400000, "value": 274.4828796386719},
-                    {"timestamp": 1542420000000, "value": 274.01922607421875},
-                    {"timestamp": 1542423600000, "value": 273.8811340332031},
-                    {"timestamp": 1542427200000, "value": 273.7162628173828},
-                    {"timestamp": 1542430800000, "value": 273.4994201660156},
-                    {"timestamp": 1542434400000, "value": 273.3451385498047},
-                    {"timestamp": 1542438000000, "value": 273.2210388183594},
-                    {"timestamp": 1542441600000, "value": 273.4631805419922},
-                    {"timestamp": 1542445200000, "value": 274.3274230957031},
-                    {"timestamp": 1542448800000, "value": 275.4784393310547},
-                    {"timestamp": 1542452400000, "value": 276.50999450683594},
-                    {"timestamp": 1542456000000, "value": 277.6051940917969},
-                    {"timestamp": 1542459600000, "value": 278.2355194091797},
-                    {"timestamp": 1542463200000, "value": 278.0879669189453},
-                    {"timestamp": 1542466800000, "value": 277.3506317138672},
-                    {"timestamp": 1542470400000, "value": 276.5809631347656},
-                    {"timestamp": 1542474000000, "value": 275.99607849121094},
-                    {"timestamp": 1542477600000, "value": 275.36585998535156},
-                    {"timestamp": 1542481200000, "value": 274.8233642578125},
-                    {"timestamp": 1542484800000, "value": 274.35382080078125},
-                    {"timestamp": 1542488400000, "value": 273.8891143798828},
-                    {"timestamp": 1542492000000, "value": 273.6193542480469},
-                    {"timestamp": 1542495600000, "value": 273.1964416503906},
-                    {"timestamp": 1542499200000, "value": 272.87730407714844},
-                ],
-            },
-            {
-                "label": "directHorizontalIrradiance",
-                "unit": "W/m²",
-                "timeseries": [
-                    {"timestamp": 1542412800000, "value": 0},
-                    {"timestamp": 1542416400000, "value": 0},
-                    {"timestamp": 1542420000000, "value": 0},
-                    {"timestamp": 1542423600000, "value": 0},
-                    {"timestamp": 1542427200000, "value": 0},
-                    {"timestamp": 1542430800000, "value": 0},
-                    {"timestamp": 1542434400000, "value": 0},
-                    {"timestamp": 1542438000000, "value": 0.01708984375},
-                    {"timestamp": 1542441600000, "value": 32.473876953125},
-                    {"timestamp": 1542445200000, "value": 106.16259765625},
-                    {"timestamp": 1542448800000, "value": 172.1171875},
-                    {"timestamp": 1542452400000, "value": 207.6816406250005},
-                    {"timestamp": 1542456000000, "value": 203.74609375089045},
-                    {"timestamp": 1542459600000, "value": 161.24023440730758},
-                    {"timestamp": 1542463200000, "value": 92.67578142064986},
-                    {"timestamp": 1542466800000, "value": 21.17968753655803},
-                    {"timestamp": 1542470400000, "value": 0.013671875010348028},
-                    {"timestamp": 1542474000000, "value": -0.011718749990169372},
-                    {"timestamp": 1542477600000, "value": 0.027343750008278423},
-                    {"timestamp": 1542481200000, "value": 0.003906250007761022},
-                    {"timestamp": 1542484800000, "value": -0.013671874993791184},
-                    {"timestamp": 1542488400000, "value": -0.046874999994825986},
-                    {"timestamp": 1542492000000, "value": -0.07421874999482599},
-                    {"timestamp": 1542495600000, "value": -0.041015624994825986},
-                    {"timestamp": 1542499200000, "value": -0.041015624995343385},
-                ],
-            },
-            {
-                "label": "diffuseHorizontalIrradiance",
-                "unit": "W/m²",
-                "timeseries": [
-                    {"timestamp": 1542412800000, "value": 0},
-                    {"timestamp": 1542416400000, "value": 0},
-                    {"timestamp": 1542420000000, "value": 0},
-                    {"timestamp": 1542423600000, "value": 0},
-                    {"timestamp": 1542427200000, "value": 0},
-                    {"timestamp": 1542430800000, "value": 0},
-                    {"timestamp": 1542434400000, "value": 0},
-                    {"timestamp": 1542438000000, "value": 3.03173828125},
-                    {"timestamp": 1542441600000, "value": 51.814697265625},
-                    {"timestamp": 1542445200000, "value": 80.36165171861649},
-                    {"timestamp": 1542448800000, "value": 95.63700693845749},
-                    {"timestamp": 1542452400000, "value": 102.13556969165802},
-                    {"timestamp": 1542456000000, "value": 101.35517811775208},
-                    {"timestamp": 1542459600000, "value": 93.04704689979553},
-                    {"timestamp": 1542463200000, "value": 74.65015459060669},
-                    {"timestamp": 1542466800000, "value": 43.18778944015503},
-                    {"timestamp": 1542470400000, "value": -0.00856328010559082},
-                    {"timestamp": 1542474000000, "value": -0.06733894348144531},
-                    {"timestamp": 1542477600000, "value": 0.03996133804321289},
-                    {"timestamp": 1542481200000, "value": -0.05777382850646973},
-                    {"timestamp": 1542484800000, "value": 0.086669921875},
-                    {"timestamp": 1542488400000, "value": 0.07489943504333496},
-                    {"timestamp": 1542492000000, "value": -0.03650379180908203},
-                    {"timestamp": 1542495600000, "value": 0.022024869918823242},
-                    {"timestamp": 1542499200000, "value": 0.025892019271850586},
-                ],
-            },
-        ],
-    }
-    bc = {
-        # "29f11d50-f11e-46e7-ba2f-7d69f796a101": {},
-        # "723ca3b3-2a02-4356-b5b7-7b52a1d65e4b": {},
-        # "932168ed-8c4c-46e6-9eb9-25e2dec8f6c9": {},
-        # "bc5bdfd1-c537-42b1-b44d-d200ae44bfbd": {},
-        "c02f1f12-966d-4eab-9f21-dcf265ceac71": {},
-        # "c17c78f3-5100-41a8-a189-cc9c2df0e3f9": {},
-        # "c7d7cf43-63f2-450c-833d-69feaca5e05c": {},
-    }
+async def weather_forecasts_as_df(start: int, end: int):
+    station_id = 10708
+    model = "cosmo-d2"
+    model_runs = (
+        ["03", "09", "15", "21"]
+        if model.upper() == "MOSMIX"
+        else ["00", "03", "06", "09", "12", "15", "18", "21"]
+    )
+    quantities = [
+        "t_2m",
+        "aswdir_s",
+        "aswdifd_s",
+        "ws_10m",
+    ]
 
-    for key, item in bc.items():
-        tmp = eg_body
-        # tmp["modelInstanceID"] = key
-        item.update(tmp)
+    # Get ensemble forecast for weather at a DWD weather station; put data into queue
+    q_nwp = []
+    async with aiohttp.ClientSession() as session:
+        weather_forecasts = [
+            asyncio.create_task(
+                request_weather_forecast(
+                    session,
+                    station_id,
+                    {
+                        "model": model,
+                        "model-run": model_run,
+                        "quantities": ",".join(quantities),
+                        "from": start,
+                        "to": end,
+                    },
+                    q_nwp,
+                )
+            )
+            for model_run in model_runs
+        ]
+
+        logger.info("Requesting weather forecasts...")
+        await asyncio.gather(*weather_forecasts)
+
+    # Append all elements in list to dataframe
+    ids = []
+    frames = []
+    for id, body in q_nwp:
+        ids.append(str(id))
+        frames.append(timeseries_array_as_df(body))
+
+    df = pd.concat(frames, keys=ids)
+    logger.trace(f"df\n{df}")
+
+    return df
+
+
+async def get_simulation_request_bodies():
+    period = pendulum.period(
+        pendulum.datetime(2018, 11, 17).start_of("day"),
+        pendulum.datetime(2018, 11, 17).end_of("day"),
+    )  # TODO make configurable
+    start = int(period.start.format("x"))
+    end = int(period.end.format("x"))
+    output_interval = 900
+
+    # Fetch weather forecasts and store in MultiIndex-dataframe
+    df = await weather_forecasts_as_df(start, end)
+
+    # outfile = f"/home/moritz/work/projekte/designetz/software/simaas/demo/outfile.csv"
+    # df.to_csv(outfile, quoting=csv.QUOTE_NONNUMERIC)
+    # df = pd.read_csv(outfile, index_col=[0, 1])
+
+    logger.debug(f"df\n{df}")
+            },
 
     logger.trace(json.dumps(bc, indent=JSON_DUMPS_INDENT))
 
@@ -232,7 +223,13 @@ async def ensemble_forecast():
     """Execute ensemble forecast and represent as dataframe."""
 
     # Assemble data used as input for simulations
-    sim_req_bodies = get_simulation_request_bodies()
+    timer_wfc = Timer(
+        text="Overall duration for preparing weather forecasts: {:.2f} seconds",
+        logger=logger.info,
+    )
+    with timer_wfc:
+        sim_req_bodies = await get_simulation_request_bodies()
+
     ensemble_runs_total = len(sim_req_bodies)
 
     # Spawn client session, task queues, producer and workers
