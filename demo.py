226b171e133eebf16e43ae47f51162be5fd256c6
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-02-04 18:55:59 +0100

    Copy/past code for plotting behaviour of GA

--- demo.py
+++ demo.py
@@ -718,6 +718,36 @@ async def genetic_algorithm():
         )
     )
 
+    return logbook
+
+
 @task
 def demo_ga(ctx):
-    asyncio.run(genetic_algorithm())
+    logbook = asyncio.run(genetic_algorithm())
+
+    # https://deap.readthedocs.io/en/master/tutorials/basic/part3.html
+    # #some-plotting-sugar
+    gen = logbook.select("gen")
+    fit_mins = logbook.chapters["fitness"].select("min")
+    fit_maxs = logbook.chapters["fitness"].select("max")
+
+    import matplotlib.pyplot as plt
+
+    fig, ax1 = plt.subplots()
+    line1 = ax1.plot(gen, fit_mins, "b-", label="Minimum Fitness")
+    ax1.set_xlabel("Generation")
+    ax1.set_ylabel("Fitness", color="b")
+    for tl in ax1.get_yticklabels():
+        tl.set_color("b")
+
+    ax2 = ax1.twinx()
+    line2 = ax2.plot(gen, fit_maxs, "r-", label="Maximum Fitness")
+    ax2.set_ylabel("Size", color="r")
+    for tl in ax2.get_yticklabels():
+        tl.set_color("r")
+
+    lns = line1 + line2
+    labs = [l.get_label() for l in lns]
+    ax1.legend(lns, labs, loc="center right")
+
+    plt.show()
